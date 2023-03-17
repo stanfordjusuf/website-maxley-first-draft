564b47ca1abb52ed2f93c7356f0c7ee26d6c56e1
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-17 12:04:06 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    "airbnb-typescript-prettier",
+    "next",
   ],
   parserOptions: {
     ecmaFeatures: {
