e82f9dcc79eb229d2274a995650d32c67c106786
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-20 15:52:09 +0700

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -22,7 +22,7 @@ function Hero() {
                   <div className="hero-overlay bg-opacity-60"></div>
                   <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                      <h1 className="mb-5 text-5xl font-bold">
+                      <h1 className="mb-5 text-3xl font-bold">
                         #livingwithmaxley
                       </h1>
                       <p className="mb-5 text-xl" style={{ color: 'white' }}>
