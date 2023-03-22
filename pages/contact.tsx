4293baf7d16daf8b6604178063a4522cbdc0753e
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-22 17:52:52 +0700

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -29,94 +29,94 @@ export default function contact() {
                 </p>
               </div>
             </div>
-          </center>
-          <div className="grid grid-cols-3 content-center">
-            <img
-              src="https://i.ibb.co/ZgDzqQC/3.png"
-              alt=""
-              style={{ width: '20px', height: 'auto' }}
-            />
-            <div className="absolute left-55 pl-6">
-              <p className="text-xl">
-                BSD:{" "}
-                <a
-                  href="https://linktr.ee/maxleysuites"
-                  className="hover:underline"
-                >
-                  Whatsapp
-                </a>
-              </p>
+            <div className="grid grid-cols-3 content-center">
+              <img
+                src="https://i.ibb.co/ZgDzqQC/3.png"
+                alt=""
+                style={{ width: '20px', height: 'auto' }}
+              />
+              <div className="absolute left-55 pl-6">
+                <p className="text-xl">
+                  BSD:{" "}
+                  <a
+                    href="https://linktr.ee/maxleysuites"
+                    className="hover:underline"
+                  >
+                    Whatsapp
+                  </a>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="grid grid-cols-3 content-center">
-            <img
-              src="https://i.ibb.co/d2vBPhL/1.png"
-              alt=""
-              style={{ width: '20px', height: 'auto' }}
-            />
-            <div className="absolute left-55 pl-6">
-              <p className="text-xl">
-                <a
-                  href="https://www.instagram.com/maxleysuites/"
-                  className="hover:underline"
-                >
-                  IG: @maxleysuites
-                </a>
-              </p>
+            <div className="grid grid-cols-3 content-center">
+              <img
+                src="https://i.ibb.co/d2vBPhL/1.png"
+                alt=""
+                style={{ width: '20px', height: 'auto' }}
+              />
+              <div className="absolute left-55 pl-6">
+                <p className="text-xl">
+                  <a
+                    href="https://www.instagram.com/maxleysuites/"
+                    className="hover:underline"
+                  >
+                    IG: @maxleysuites
+                  </a>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="grid grid-cols-3 content-center">
-            <img
-              src="https://i.ibb.co/LvNw1Dv/2.png"
-              alt=""
-              style={{ width: '20px', height: 'auto' }}
-            />
-            <div className="absolute left-55 pl-6">
-              <p className="text-xl">
-                <a
-                  href="https://www.tiktok.com/@maxleysuites?_t=8aRWNgJ7DDm&_r=1"
-                  className="hover:underline"
-                >
-                  TikTok: @maxleysuites
-                </a>
-              </p>
+            <div className="grid grid-cols-3 content-center">
+              <img
+                src="https://i.ibb.co/LvNw1Dv/2.png"
+                alt=""
+                style={{ width: '20px', height: 'auto' }}
+              />
+              <div className="absolute left-55 pl-6">
+                <p className="text-xl">
+                  <a
+                    href="https://www.tiktok.com/@maxleysuites?_t=8aRWNgJ7DDm&_r=1"
+                    className="hover:underline"
+                  >
+                    TikTok: @maxleysuites
+                  </a>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="grid grid-cols-3 content-center">
-            <img
-              src="https://i.ibb.co/ChdTZp1/5.png"
-              alt=""
-              style={{ width: '20px', height: 'auto' }}
-            />
-            <div className="absolute left-55 pl-6">
+            <div className="grid grid-cols-3 content-center">
+              <img
+                src="https://i.ibb.co/ChdTZp1/5.png"
+                alt=""
+                style={{ width: '20px', height: 'auto' }}
+              />
+              <div className="absolute left-55 pl-6">
+                <p className="text-xl">
+                  <a
+                    href="mailto:maxleysuites@gmail.com"
+                    className="hover:underline"
+                  >
+                    Email: maxleysuites@gmail.com
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-3 content-center">
               <p className="text-xl">
                 <a
                   href="mailto:maxleysuites@gmail.com"
                   className="hover:underline"
                 >
-                  Email: maxleysuites@gmail.com
+                  Partnership: maxleysuites@gmail.com
                 </a>
               </p>
             </div>
+            <br />
+            <a href="https://goo.gl/maps/SAxM51p66Yasbj2L9">
+              <img
+                src="https://i.ibb.co/QXrSr8t/Screenshot-2023-03-19-at-14-40-46.png"
+                className="max-w-sm rounded-lg shadow-2xl"
+              />
+            </a>
           </div>
-          <div className="grid grid-cols-3 content-center">
-            <p className="text-xl">
-              <a
-                href="mailto:maxleysuites@gmail.com"
-                className="hover:underline"
-              >
-                Partnership: maxleysuites@gmail.com
-              </a>
-            </p>
-          </div>
-          <br />
-          <a href="https://goo.gl/maps/SAxM51p66Yasbj2L9">
-            <img
-              src="https://i.ibb.co/QXrSr8t/Screenshot-2023-03-19-at-14-40-46.png"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
-          </a>
-        </div>
+        <center/>
       </div>
     </div>
   );
