527d9c4b1efd88726ab906f5e288043cd5946fb8
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-22 18:18:51 +0700

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -158,52 +158,109 @@ function Hero() {
           <div className="hero-content flex-col lg:flex-row-reverse">
             <img
               src="https://i.ibb.co/tH6zr8h/Screenshot-2023-03-19-at-14-51-17.png"
-              className="max-w-sm rounded-lg shadow-2xl"
+              className="lg:max-w-3xl max-w-sm rounded-lg shadow-3xl"
             />
             <div>
               <h1 className="text-5xl font-bold" style={{ color: 'white' }}>
                 Contact Us!
               </h1>
               <br />
-              <p className="text-xl">
-                Email:{" "}
-                <a
-                  href="mailto:maxleysuites@gmail.com"
-                  className="hover:underline"
-                >
-                  maxleysuites@gmail.com
-                </a>
-              </p>
-              <p className="text-xl">
-                BSD:{" "}
-                <a
-                  href="https://wa.me/+6288225924227"
-                  className="hover:underline"
-                >
-                  +6288225924227
-                </a>
-              </p>
-              <p className="text-xl">
-                Lippo:{" "}
-                <a
-                  href="https://wa.me/+6285852293500"
-                  className="hover:underline"
-                >
-                  +6285852293500
-                </a>
-              </p>
-              <p className="text-xl">
-                Customer Service and Feedback:{" "}
-                <a
-                  href="https://wa.me/+6281227762299"
-                  className="hover:underline"
-                >
-                  +6281227762299
-                </a>
-              </p>
-              <p className="text-xl font-bold">
-                Operational Hours: 09.00 - 17.00 WIB
-              </p>
+              <div className="grid grid-cols-3 content-center">
+                <img
+                  src="https://i.ibb.co/7WgCFf1/svg.png"
+                  alt=""
+                  style={{ width: '20px', height: 'auto' }}
+                />
+                <div className="absolute left-55 pl-6">
+                  <p className="text-xl">
+                    Operational Hours: 09.00 - 17.00 WIB
+                  </p>
+                </div>
+              </div>
+              <br />
+              <div className="grid grid-cols-3 content-center">
+                <img
+                  src="https://i.ibb.co/ZgDzqQC/3.png"
+                  alt=""
+                  style={{ width: '20px', height: 'auto' }}
+                />
+                <div className="absolute left-55 pl-6">
+                  <p className="text-xl">
+                    BSD:{" "}
+                    <a
+                      href="https://linktr.ee/maxleysuites"
+                      className="hover:underline"
+                    >
+                      Whatsapp
+                    </a>
+                  </p>
+                </div>
+              </div>
+              <br />
+              <div className="grid grid-cols-3 content-center">
+                <img
+                  src="https://i.ibb.co/d2vBPhL/1.png"
+                  alt=""
+                  style={{ width: '20px', height: 'auto' }}
+                />
+                <div className="absolute left-55 pl-6">
+                  <p className="text-xl">
+                    <a
+                      href="https://www.instagram.com/maxleysuites/"
+                      className="hover:underline"
+                    >
+                      IG: @maxleysuites
+                    </a>
+                  </p>
+                </div>
+              </div>
+              <br />
+              <div className="grid grid-cols-3 content-center">
+                <img
+                  src="https://i.ibb.co/LvNw1Dv/2.png"
+                  alt=""
+                  style={{ width: '20px', height: 'auto' }}
+                />
+                <div className="absolute left-55 pl-6">
+                  <p className="text-xl">
+                    <a
+                      href="https://www.tiktok.com/@maxleysuites?_t=8aRWNgJ7DDm&_r=1"
+                      className="hover:underline"
+                    >
+                      TikTok: @maxleysuites
+                    </a>
+                  </p>
+                </div>
+              </div>
+              <br />
+              <div className="grid grid-cols-3 content-center">
+                <img
+                  src="https://i.ibb.co/ChdTZp1/5.png"
+                  alt=""
+                  style={{ width: '20px', height: 'auto' }}
+                />
+                <div className="absolute left-55 pl-6">
+                  <p className="text-xl">
+                    <a
+                      href="mailto:maxleysuites@gmail.com"
+                      className="hover:underline"
+                    >
+                      Email: maxleysuites@gmail.com
+                    </a>
+                  </p>
+                </div>
+              </div>
+              <br />
+              <div className="grid grid-cols-3 content-center">
+                <p className="text-xl">
+                  <a
+                    href="mailto:maxleysuites@gmail.com"
+                    className="hover:underline"
+                  >
+                    Partnership: maxleysuites@gmail.com
+                  </a>
+                </p>
+              </div>
               <br />
               <a href="https://goo.gl/maps/SAxM51p66Yasbj2L9">
                 <img
