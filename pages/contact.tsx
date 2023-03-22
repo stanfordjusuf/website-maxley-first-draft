994cf5c6afcd0a32f8faf6c2ec35aa5d23caf0c6
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-22 17:31:13 +0700

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -22,7 +22,7 @@ export default function contact() {
               alt=""
               style={{ width: '20px', height: 'auto' }}
             />
-            <div className="absolute left-55 pl-5">
+            <div className="absolute left-55 pl-6">
               <p className="text-xl">
                 Email:{" "}
                 <a
