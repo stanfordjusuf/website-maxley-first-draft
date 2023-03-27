230ae2c8e517865eb87fd3b9aa7287cd4276b650
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-27 15:16:33 +0700

    Update services.tsx

--- pages/services.tsx
+++ pages/services.tsx
@@ -21,6 +21,10 @@ export default function services() {
                       className="w-full"
                       alt="Tailwind CSS Carousel component"
                     />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                      <a href="#slide4" className="btn btn-circle">❮</a> 
+                      <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
                   </div>
                   <div className="carousel-item w-full">
                     <img
