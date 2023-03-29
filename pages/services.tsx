6549c91e0df94bb6dc23c32f747e2eade65a47ef
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-29 15:14:42 +0700

    Update services.tsx

--- pages/services.tsx
+++ pages/services.tsx
@@ -62,8 +62,8 @@ export default function services() {
                 </div>
               </div>
             </div>
+            <br />
           </div>
-          <br />
           <div className="flex flex-col items-center">
             <div className="card w-3/4 bg-base-100 shadow-xl h-3/4">
               <div className="card-body">
