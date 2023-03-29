821ef4c1ded6f389b0d816e8c12cb2ce73261656
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-29 15:33:51 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -26,61 +26,59 @@ const Footer = () => (
           </a>
         </Link>
       </div>
-      <center>
-        <div className="justify-self-center">
-          <Link href="/contact">
-            <a href="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer">
-              Contact
-            </a>
-          </Link>
-          <div className="grid grid-flow-col gap-4">
-            <a href="https://www.instagram.com/maxleysuites/">
-              <img
-                src="https://i.ibb.co/d2vBPhL/1.png"
-                alt=""
-                style={{ width: '20px', height: 'auto' }}
-              />
-            </a>
-            <a href="https://www.tiktok.com/@maxleysuites?_t=8aRWNgJ7DDm&_r=1">
-              <img
-                src="https://i.ibb.co/LvNw1Dv/2.png"
-                alt=""
-                style={{ width: '20px', height: 'auto' }}
-              />
-            </a>
-            <a href="https://api.whatsapp.com/send/?phone=6285852293500&text&type=phone_number&app_absent=0">
-              <img
-                src="https://i.ibb.co/ZgDzqQC/3.png"
-                alt=""
-                style={{ width: '20px', height: 'auto' }}
-              />
-              <p>Lippo</p>
-            </a>
-            <a href="https://api.whatsapp.com/send/?phone=6288225924227&text&type=phone_number&app_absent=0">
-              <img
-                src="https://i.ibb.co/ZgDzqQC/3.png"
-                alt=""
-                style={{ width: '20px', height: 'auto' }}
-              />
-              <p>BSD</p>
-            </a>
-            <a href="https://goo.gl/maps/SAxM51p66Yasbj2L9">
-              <img
-                src="https://i.ibb.co/fdrPTwD/4.png"
-                alt=""
-                style={{ width: '20px', height: 'auto' }}
-              />
-            </a>
-            <a href="mailto:maxleysuites@gmail.com">
-              <img
-                src="https://i.ibb.co/ChdTZp1/5.png"
-                alt=""
-                style={{ width: '20px', height: 'auto' }}
-              />
-            </a>
-          </div>
+      <div className="justify-self-center ">
+        <Link href="/contact">
+          <a href="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer">
+            Contact
+          </a>
+        </Link>
+        <div className="grid grid-flow-col gap-4">
+          <a href="https://www.instagram.com/maxleysuites/">
+            <img
+              src="https://i.ibb.co/d2vBPhL/1.png"
+              alt=""
+              style={{ width: '20px', height: 'auto' }}
+            />
+          </a>
+          <a href="https://www.tiktok.com/@maxleysuites?_t=8aRWNgJ7DDm&_r=1">
+            <img
+              src="https://i.ibb.co/LvNw1Dv/2.png"
+              alt=""
+              style={{ width: '20px', height: 'auto' }}
+            />
+          </a>
+          <a href="https://api.whatsapp.com/send/?phone=6285852293500&text&type=phone_number&app_absent=0">
+            <img
+              src="https://i.ibb.co/ZgDzqQC/3.png"
+              alt=""
+              style={{ width: '20px', height: 'auto' }}
+            />
+            <p>Lippo</p>
+          </a>
+          <a href="https://api.whatsapp.com/send/?phone=6288225924227&text&type=phone_number&app_absent=0">
+            <img
+              src="https://i.ibb.co/ZgDzqQC/3.png"
+              alt=""
+              style={{ width: '20px', height: 'auto' }}
+            />
+            <p>BSD</p>
+          </a>
+          <a href="https://goo.gl/maps/SAxM51p66Yasbj2L9">
+            <img
+              src="https://i.ibb.co/fdrPTwD/4.png"
+              alt=""
+              style={{ width: '20px', height: 'auto' }}
+            />
+          </a>
+          <a href="mailto:maxleysuites@gmail.com">
+            <img
+              src="https://i.ibb.co/ChdTZp1/5.png"
+              alt=""
+              style={{ width: '20px', height: 'auto' }}
+            />
+          </a>
         </div>
-      </center>
+      </div>
       <div className="justify-self-center">
         <p>Feedback</p>
         <p className="link link-hover">081227762299</p>
